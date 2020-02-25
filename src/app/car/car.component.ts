8a74851dc8387138165a0d6e8c8f4aa58f7c2666
Gaukhar <sundetkaliyeva999@gmail.com>
2020-02-25 13:06:20 +0600

    test

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  template: '<h2>Single car</h2>',
+  styleUrls: ['./car.component.scss']
+})
+export class CarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
